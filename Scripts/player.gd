d1b0c3e666db42690aa0d171c877650cecf63a74
HaviarA1 <haviar.antonin@gmail.com>
2026-02-27 15:27:17 +0100

    Added player movement

---
+++ Scripts/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var fall_acceleration = 0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	# Handle jump.
+	if is_on_floor():
+		fall_acceleration = 0
+		if Input.is_action_just_pressed("up"):
+			velocity.y = JUMP_VELOCITY	
+	else:
+		velocity.y += fall_acceleration
+		fall_acceleration += 0.1
+	
+	if Input.is_action_pressed("left"):
+		velocity.x = -200
+	elif Input.is_action_pressed("right"):
+		velocity.x = 200
+	elif abs(velocity.x) > 10:
+		if velocity.x < 0:
+			velocity.x += 10
+		else:
+			velocity.x -= 10
+	else:
+		velocity.x = 0	
+		
+		
+
+	move_and_slide()
