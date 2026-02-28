1f0e81a96d7da720f208307a6097d5df57ff39ad
HaviarA1 <haviar.antonin@gmail.com>
2026-02-28 13:56:30 +0100

    MainMenu

--- Scripts/custom_animation.gd
+++ Scripts/custom_animation.gd
@@ -1,12 +1,28 @@
 extends Node2D
-
+@onready var sprite_2d: Sprite2D = $Sprite2D
 @export var frames = []
+@export var slide_time := 1.0
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func intro():
+	for n in range(0, frames.size()):
+		sprite_2d.texture = frames[n]
+		animation_player.play("lighten")
+		await get_tree().create_timer(3).timeout
+		animation_player.play("darken")
+		await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	intro()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_skip_pressed() -> void:
+	animation_player.play("darken")
+	await get_tree().create_timer(3).timeout
+	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
---
+++ Scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_texture_button_pressed() -> void:
+	animation_player.play("darken")
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://Scenes/custom_animation.tscn")
