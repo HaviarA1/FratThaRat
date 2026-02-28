aca71228fc4f9bcd6025e8120a689bfa0604313c
HaviarA1 <haviar.antonin@gmail.com>
2026-02-28 11:43:43 +0100

    Update hermelin.gd

--- Scripts/hermelin.gd
+++ Scripts/hermelin.gd
@@ -9,12 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-	
-	
-
-
-
 
 func _on_collect_body_entered(body: Node2D) -> void:
 
