c924e6bd15b7d64771d121193d379fd252e2fde5
HaviarA1 <haviar.antonin@gmail.com>
2026-02-28 14:29:54 +0100

    Ending and fixed not working transitions

--- Scripts/ending_controller.gd
+++ Scripts/ending_controller.gd
@@ -1,10 +1,16 @@
 extends Node2D
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var audio_stream_player: AudioStreamPlayer = $"../AudioStreamPlayer"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func ending():
+	animation_player.play("ending")
+	await get_tree().create_timer(3).timeout
+	audio_stream_player.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
